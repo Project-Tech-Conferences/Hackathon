0265bd5d3193f005088694135bba7fbd069a1409
Seneli Seneviratne <seneliseneviratne@cmail.carleton.ca>
2021-01-07 11:39:00 -0500

    styled header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,40 +7,13 @@ const Header = () => {
     <div className="section" id="home">
       <div className="container">
         <div className="header-wrapper">
-          {/* <Fade bottom>
-            <h2>
-              Hi, I'm {data.name}{" "}
-              <span role="img" aria-label="Emoji">
-                👋
-              </span>
-            </h2>
-          </Fade> */}
           <Fade bottom cascade>
+            {
             <div className="heading-wrapper">
-              <h1>
-                {data.headerTagline[0]
-                  ? data.headerTagline[0]
-                  : "Building digital"}
-              </h1>
-              <h1>
-                {" "}
-                {data.headerTagline[1]
-                  ? data.headerTagline[1]
-                  : "products, brands"}
-              </h1>
-              <h1>
-                {" "}
-                {data.headerTagline[2]
-                  ? data.headerTagline[2]
-                  : "and experience"}
-              </h1>
-            </div>
-          </Fade>
-          <Fade bottom>
-            <p>{data.headerParagraph}</p>
-          </Fade>
-          <Fade bottom>
-            <a
+              <h1 className="title">PTC HACKS</h1>
+              <h3 className="description">hackathon for highschool students</h3>
+              <h2 className='date'>February 19th-21st</h2>
+              <a
               href={`mailto:${
                 data.contactEmail ? data.contactEmail : "hello@chetanverma.com"
               }`}
@@ -48,6 +21,8 @@ const Header = () => {
             >
               Apply
             </a>
+            </div>
+            }
           </Fade>
         </div>
       </div>
