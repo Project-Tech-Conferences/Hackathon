d7ef78ed0cb64ac61d635ab96d11563b0c4aad70
Youngjun <yjl.2000@hotmail.com>
2021-01-15 01:12:32 -0500

    changed google fonts, fixed countdown, challenges -> prizes

---
+++ .history/src/components/Challenges_20210115005400.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+const Challenges = () => {
+  return (
+    <div className="section" id="challenges">
+      <div className="container">
+        <div className="skills-container">
+          <h1>prizes</h1>
+          <div className="skills-grid">
+            <div className="challengeBox">
+              <p>1st place</p>
+              <p className=>$1900</p>
+            </div>
+            <div className="challengeBox">
+              <p>2nd place</p>
+              <p>$1700</p>
+            </div>
+            <div className="challengeBox">
+              <p>3rd place</p>
+              <p>$1600</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Challenges
