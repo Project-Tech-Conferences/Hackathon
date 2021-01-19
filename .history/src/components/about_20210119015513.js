50baac844d4e76e52472feb5c26c7ad755fe50e9
Youngjun <yjl.2000@hotmail.com>
2021-01-19 03:37:06 -0500

    remade schedule component and faq

---
+++ .history/src/components/about_20210119015513.js
@@ -0,0 +1,29 @@
+import React from "react"
+import Fade from "react-reveal/Fade"
+import data from "../yourdata"
+
+const About = () => {
+  return (
+    <div className="section" id="about">
+      <div className="container">
+        <div className="about-section">
+          <div className="content-container">
+          <Fade bottom cascade>
+            <h1>
+              Code for <br className="break"></br>Covid
+            </h1>
+          </Fade>
+          <p className="about-content">{data.aboutParaOne}</p>
+          <a
+            className="signUp"
+            href="https://projecttc.typeform.com/to/myGJeoEM"
+          >
+            SIGN UP
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
